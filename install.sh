defbdedbe49593427903ea5321cd083b65d96691
David Lane <git2018@dmlane.net>
2020-09-19 17:59:04 +0200

    Added libmariadb-dev package

--- install.sh
+++ install.sh
@@ -10,13 +10,15 @@ readlinkf(){ perl -MCwd -e 'print Cwd::abs_path shift' "$1";}
 fail() { echo "$1"; exit 1;}
 macos() { test $(uname -s) == Darwin;}
 function apt_install {
-	if [ ! -z "$2" ] ; then
-		test -f $2 && return
-	fi
-	echo "Installing $1"
-	sleep 1
-	sudo apt -y install $1
+    dpkg -s $1 >/dev/null 2>&1
+    test $? -eq 0 && return 0
+    echo "Installing $1"
+    sleep 1
+    test -f /tmp/.install.flag || sudo apt-get update
+    touch /tmp/.install.flag
+    sudo apt -y install $1
 }
+
 function brew_install {
 	if [ ! -z "$2" ] ; then
 		test -f $2 && return
@@ -30,7 +32,8 @@ function brew_install {
 function linux_packages {
 	# perldoc can be  a place holder which always fails - CPAN should always 
 	# exist, so this should work ....
-	perldoc -l CPAN >/dev/null || apt_install perl-doc 
+	apt_install perl-doc 
+	apt_install libmariadb-dev
 }
 function mac_packages {
 	test -f /usr/local/bin/brew ||\
